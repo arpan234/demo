a75e278c92d711a0aa7224a12adad1a46ff094f6
Arpan Patel <arpanpatel_234@yahoo.com>
2018-06-05 12:13:54 -0700

    commented logic

--- swap.js
+++ swap.js
@@ -4,9 +4,9 @@ var b = 10;
 console.log("before a is: " +a);
 console.log("before b is: " +b);
 
-var temp = x;
-x = y;
-y = temp;
+// var temp = x;
+// x = y;
+// y = temp;
 
 console.log("now a is: " +a);
 console.log("now b is: " +b);
