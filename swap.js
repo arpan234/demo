2d4400684c58929cf90d03c31afe229999dca50c
Arpan Patel <arpanpatel_234@yahoo.com>
2018-06-05 11:40:33 -0700

    swap logic added

--- swap.js
+++ swap.js
@@ -4,7 +4,9 @@ var b = 10;
 console.log("before a is: " +a);
 console.log("before b is: " +b);
 
-//swapping
+var temp = x;
+x = y;
+y = temp;
 
 console.log("now a is: " +a);
 console.log("now b is: " +b);
